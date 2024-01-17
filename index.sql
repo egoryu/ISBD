977886fb4b47d5e9fb2167df29bd676cd044c87b
Egor Nikitin <egornikitin1401@gmail.com>
2024-01-17 16:05:33 +0300

    index

--- index.sql
+++ index.sql
@@ -1,19 +1,9 @@
-CREATE INDEX ON "user" USING HASH ("id");
-CREATE INDEX ON "user" USING HASH ("age");
-CREATE INDEX ON "user" USING BTREE ("registration_date");
+CREATE INDEX ON "country_company" USING BTREE ("country_id", "company_id");
 
-CREATE INDEX ON "portfolio" USING HASH ("user_id");
+CREATE INDEX ON "trading_signal" USING HASH ("type");
 
-CREATE INDEX ON "stock" USING HASH ("category_id");
+CREATE INDEX ON "stock_property_value" USING BTREE("stock_category_property_id", "stock_id");
+
+CREATE INDEX ON "stock_category_property" USING BTREE ("stock_category_id");
+
 CREATE INDEX ON "stock" USING HASH ("currency_id");
-
-CREATE INDEX ON "portfolio_item" USING HASH ("portfolio_id");
-
-CREATE INDEX ON "trading_signal" USING HASH ("stock_id");
-CREATE INDEX ON "trading_signal" USING BTREE ("signal_time");
-
-CREATE INDEX ON "stock_price" USING HASH ("stock_id");
-CREATE INDEX ON "stock_price" USING BTREE ("date");
-
-CREATE INDEX ON "stock_predication" USING HASH ("stock_id");
-CREATE INDEX ON "stock_predication" USING BTREE ("date");
